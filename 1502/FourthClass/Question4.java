78f4e4ab9c65a0ec79a9638457eae62a0b53cb27
sooshh <otterpesce@gmail.com>
2026-01-27 11:41:48 -0700

    ksdhfkwje

--- 1502/FourthClass/Question4.java
+++ 1502/FourthClass/Question4.java
@@ -5,9 +5,9 @@ public class Question4 {
     public static void main(String [] args) {
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Enter a number");
-        //String num = keyboard.next(); //num = "123"
-        int iNum = keyboard.nextInt(); // num = 123
-        //int iNum = Integer.parseInt(num); //num = 123
+        String num = keyboard.next(); //num = "123"
+        //int iNum = keyboard.nextInt(); // num = 123
+        int iNum = Integer.parseInt(num); //num = 123
         iNum = iNum*3; //num = 369
         System.out.println("value: " + iNum); // "value: 369"
     }
---
+++ 1502/January27/src/Player.java
@@ -0,0 +1,108 @@
+import java.time.LocalDate;
+
+public class Player {
+    private int jerseyNum;
+    private String name;
+    private double height;
+    private double weight;
+    private LocalDate dateOfBirth;
+    private String team;
+
+    //Contructors:
+    public Player() {
+        this.jerseyNum = 67;
+        this.name = "Nelson";
+        dateOfBirth = LocalDate.now();
+    }
+    public Player(int jersey, String name) {
+        this.jerseyNum = jersey;
+        this.name = name;
+        dateOfBirth = LocalDate.now();
+    }
+
+    public Player(String name, int jersey) {
+        this.name = name;
+        jerseyNum = jersey;
+        dateOfBirth = LocalDate.now();
+    }
+    public Player(int jersey, String name, double height, double weight) {
+        jerseyNum = jersey;
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+        dateOfBirth = LocalDate.now();
+    }
+    public Player(int jersey, String name, double height, double weight, LocalDate dob) {
+        jerseyNum = jersey;
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+        dateOfBirth = dob;
+    }
+    public Player(int jersey, String name, double height, double weight, LocalDate dob, String team) {
+        jerseyNum = jersey;
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+        dateOfBirth = dob;
+        this.team = team;
+    }
+    //public Player(int jerseyNum, String name, double weight, double height) {
+        //this.jerseyNum = jerseyNum;
+        //this.name = name;
+        //this.height = height;
+        //this.weight = weight;
+    //}
+
+
+    //Setters and Getters:
+    public void setJerseyNum(int jersey) {
+        this.jerseyNum = jersey;
+    }
+    public int getJerseyNum() {
+        return this.jerseyNum;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return this.name;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+    public Double getHeight() {
+        return this.height;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+    public double getWeight() {
+        return this.weight;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+    public LocalDate getDateOfBirth() {
+        return this.dateOfBirth;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+    public String getTeam() {
+        return this.team;
+    }
+
+    //Methods:
+    public int getAge() {
+        //return -1;
+        int currentYr = LocalDate.now().getYear();
+        int birthYr = getDateOfBirth().getYear();
+        return (currentYr-birthYr);
+    }
+}
