2b9b67ceb15fe8811be056d1622ea4fbe5674ef3
sooshh <otterpesce@gmail.com>
2026-02-03 12:40:20 -0700

    reeeeeee

--- 1502/AppFeb3/src/InteractivePlayer.java
+++ 1502/AppFeb3/src/InteractivePlayer.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class InteractivePlayer {
@@ -8,14 +9,44 @@ public class InteractivePlayer {
         System.out.println("How many players do you want?"); //prompt
         int numPlayers = keyboard.nextInt();
         //System.out.println(numPlayers);
+        
+        ArrayList <Player> playerList = new ArrayList<Player>();
+
         for (int i=0; i < numPlayers; i++) {
             //System.out.println(i); 
             System.out.println("What should player " +(i+1)+"'s name be?");
             String playerLoopName = keyboard.next();
             System.out.println("What should player " +(i+1)+"'s jersey number be?");
             int playerLoopJNum = keyboard.nextInt();
-            Player nelson = new Player(playerLoopName,playerLoopJNum);
-            System.out.println(nelson); 
+            Player newPlayer = new Player(playerLoopName,playerLoopJNum);
+            //System.out.println(newPlayer); 
+            playerList.add(newPlayer);
         }
+
+        //System.out.println(playerList); 
+        
+
+        for(Player processPlayer : playerList) {
+            //prompt and input for team name
+            System.out.println("What should " + processPlayer.getName() + "'s team be?");
+            processPlayer.setTeam(keyboard.next());
+
+            //prompts and inputs for date of birth
+            System.out.println("What should " + processPlayer.getName() + "'s birth YEAR be?");
+            int playerBirthYear = keyboard.nextInt();
+
+            System.out.println("What should " + processPlayer.getName() + "'s birth MONTH be?");
+            int playerBirthMonth = keyboard.nextInt();
+
+            System.out.println("What should " + processPlayer.getName() + "'s birth DAY be?");
+            int playerBirthDay = keyboard.nextInt();
+
+            //setting date of birth thru LocalDate.of, taken from prev lab
+            processPlayer.setDateOfBirth(LocalDate.of(playerBirthYear, playerBirthMonth, playerBirthDay));
+        } 
+        
+        keyboard.close();
+
+        System.out.println(playerList); 
     }
 }
